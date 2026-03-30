b43f4d3361da74ec64fde0d1b4d20af83893ec4c
viveka87 <viveka87.s@gmail.com>
2026-03-30 14:17:17 +0530

    Connected Django project to MySQL database

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist_django',        # The name of the database you created in phpMyAdmin
+        'USER': 'root',           # Your MySQL username (default is root)
+        'PASSWORD': 'test@123',           # Your MySQL password (default is empty for XAMPP)
+        'HOST': '127.0.0.1',      # Or 'localhost'
+        'PORT': '3306',
+        
     }
 }
 
